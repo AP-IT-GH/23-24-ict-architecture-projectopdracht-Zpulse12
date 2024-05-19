07e6cb1ed40266b2d7fc35b9c964be4215125720
zpulse12 <Haitam.baqloul@hotmail.com>
2024-05-19 23:55:24 +0000

    add better error handling

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const apiRoute = require('./api.route');
 
 const app = express();
+app.use(cors()); // Add this line to enable CORS
 
 app.use('/api', apiRoute);
 
 app.use(express.static('public'));
-
-
 
 app.listen(3000, () => {
     console.log('Upload app listening on port 3000!');
